cc3fc24e74e07ecb8e94d3d4647e50fc95e7e946
Satria Primatama <satriaprimatama88@gmail.com>
2023-01-20 23:15:26 +0700

    Update Client Side, Controller, and View

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -1,8 +1,13 @@
 ﻿using Api.Base;
 using Api.Models;
 using Api.Repositories.Data;
+using Api.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Api.Controllers
 {
@@ -10,6 +15,8 @@ namespace Api.Controllers
     [ApiController]
     public class AccountController : BaseController<AccountRepositories, Accounts, int>
     {
+        private AccountRepositories _repo;
+        private IConfiguration _con;
         public AccountController(AccountRepositories repo) : base(repo)
         {
         }
--- Api/Repositories/Data/AccountRepositories.cs
+++ Api/Repositories/Data/AccountRepositories.cs
@@ -1,12 +1,20 @@
 ﻿using Api.Contexts;
+using Api.Handlers;
 using Api.Models;
+using Api.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace Api.Repositories.Data
 {
     public class AccountRepositories : GeneralRepository<Accounts, int>
     {
+        private MyContext _context;
+        private DbSet<Accounts> _accounts;
+
         public AccountRepositories(MyContext context) : base(context)
         {
+            _context = context;
+            _accounts = context.Set<Accounts>();
         }
     }
 }
