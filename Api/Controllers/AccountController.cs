4db13ee2f9651e07fa5726dbcf3008187614d6c7
Ghifari80 <ghifariauliarutfi@gmail.com>
2023-01-20 11:10:56 +0700

    Login
    TestLogin

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -1,8 +1,14 @@
 ﻿using Api.Base;
 using Api.Models;
 using Api.Repositories.Data;
+using Api.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace Api.Controllers
 {
@@ -10,8 +16,39 @@ namespace Api.Controllers
     [ApiController]
     public class AccountController : BaseController<AccountRepositories, Accounts, int>
     {
-        public AccountController(AccountRepositories repo) : base(repo)
+        private AccountRepositories _repo;
+        private IConfiguration _con;
+        public AccountController(AccountRepositories repo, IConfiguration con) : base(repo)
         {
+            _repo = repo;
+            _con = con;
+        }
+        [HttpPost]
+        [Route("Login")]
+        [AllowAnonymous]
+        public ActionResult Login(LoginVM loginVM)
+        {
+            try
+            {
+                var result = _repo.Login(loginVM);
+                switch (result)
+                {
+                    case 0:
+                        return Ok(new { statusCode = 200, message = "Account Not Found!" });
+                    case 1:
+                        return Ok(new { statusCode = 200, message = "Wrong Password!" });
+                    case 2:
+                        return Ok(new { statusCode = 200, message = "Login Success!"});
+
+                    default:
+                      return Ok(new { statusCode = 401, message = "Login Gagal!"});
+                }
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { statusCode = 500, message = $"Something Wrong! : {e.Message}" });
+            }
+
         }
     }
 }
--- Api/Repositories/Data/AccountRepositories.cs
+++ Api/Repositories/Data/AccountRepositories.cs
@@ -1,12 +1,36 @@
 ﻿using Api.Contexts;
+using Api.Handlers;
 using Api.Models;
+using Api.ViewModels;
+
 
 namespace Api.Repositories.Data
 {
     public class AccountRepositories : GeneralRepository<Accounts, int>
     {
+        private readonly MyContext _context;
         public AccountRepositories(MyContext context) : base(context)
         {
+            _context = context;
+        }
+        public int Login (LoginVM login)
+        {
+            var result = _context.Accounts.Join(_context.Employees, a => a.EmployeeId, e => e.Id, (a, e) =>
+            new LoginVM
+            {
+                Email = a.Email,
+                Password = a.Password
+            }).SingleOrDefault(c => c.Email == login.Email);
+
+            if(result == null)
+            {
+                return 0;
+            }
+            if(!Hashing.ValidatePassword(login.Password, result.Password))
+            {
+                return 1;
+            }
+            return 2;
         }
     }
 }
