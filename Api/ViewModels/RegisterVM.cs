f987ea918cc888817b28d68a0f381219f1d547b6
Indrawan Heri Prabowo <indrawanherip@gmail.com>
2023-01-21 22:49:26 +0700

    check

---
+++ Api/ViewModels/RegisterVM.cs
@@ -0,0 +1,20 @@
+﻿using Api.Models;
+
+namespace Api.ViewModels;
+
+public class RegisterVM
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public string Address { get; set; }
+    public DateTime HireDate { get; set; }
+    public DateTime ReleaseDate { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Gender Gender { get; set; }
+    public int Age { get; set; }
+    public int Manager { get; set; }
+    public int DepartmenId { get; set; }
+    public int JobId { get; set; }
+}
