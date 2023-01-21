eba50fcac7101929770cd6ed54630a5e301a3bae
Indrawan Heri Prabowo <indrawanherip@gmail.com>
2023-01-21 22:59:10 +0700

    fix

--- Api/Controllers/AccountsController.cs
+++ Api/Controllers/AccountsController.cs
@@ -21,84 +21,6 @@ public class AccountsController : BaseController<AccountRepositories, Accounts, 
     private IConfiguration _config;
     public AccountsController(AccountRepositories repo, IConfiguration con) : base(repo)
     {
-<<<<<<< HEAD
-        private AccountRepositories _repo;
-        private IConfiguration _con;
-        public AccountController(AccountRepositories repo, IConfiguration con) : base(repo)
-        {
-            _repo = repo;
-            _con = con;
-        }
-
-        [HttpPost]
-        [Route("Register")]
-        [AllowAnonymous]
-        public ActionResult Register(RegisterVM registerVM)
-        {
-            try
-            {
-                var result = _repo.Register(registerVM);
-                return result == 0
-                    ? Ok(new { statusCode = 204, message = "Email or Phone is Already Registered!" })
-                    : Ok(new { statusCode = 201, message = "Register Succesfully!" });
-            }
-            catch (Exception e)
-            {
-                return BadRequest(new { statusCode = 500, message = $"Something Wrong! : {e.Message}" });
-            }
-        }
-
-        [HttpPost]
-        [Route("Login")]
-        [AllowAnonymous]
-        public ActionResult Login(LoginVM loginVM)
-        {
-            try
-            {
-                var result = _repo.Login(loginVM);
-                switch (result)
-                {
-                    case 0:
-                        return Ok(new { statusCode = 200, message = "Account Not Found!" });
-                    case 1:
-                        return Ok(new { statusCode = 200, message = "Wrong Password!" });
-                    default:
-                        // bikin method untuk mendapatkan role-nya user yang login
-                        var roles = _repo.UserRoles(loginVM.Email);
-
-                        var claims = new List<Claim>()
-                        {
-                        new Claim("email", loginVM.Email)
-                        };
-
-                        foreach (var item in roles)
-                        {
-                            claims.Add(new Claim(ClaimTypes.Role, item));
-                        }
-
-                        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_con["JWT:Key"]));
-                        var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-                        var token = new JwtSecurityToken(
-                            _con["JWT:Issuer"],
-                            _con["JWT:Audience"],
-                            claims,
-                            expires: DateTime.Now.AddMinutes(10),
-                            signingCredentials: signIn
-                            );
-
-                        var generateToken = new JwtSecurityTokenHandler().WriteToken(token);
-                        claims.Add(new Claim("Token Security", generateToken.ToString()));
-
-                        return Ok(new { statusCode = 200, message = "Login Success!", data = generateToken, Roles = roles, Email = loginVM.Email });
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest(new { statusCode = 500, message = $"Something Wrong! : {e.Message}" });
-            }
-
-
-=======
         _repositories = repo;
         _config = con;
     }
@@ -165,7 +87,6 @@ public class AccountsController : BaseController<AccountRepositories, Accounts, 
         catch (Exception e)
         {
             return BadRequest(new { statusCode = 500, message = $"Something Wrong! : {e.Message}" });
->>>>>>> main
         }
     }
 }
