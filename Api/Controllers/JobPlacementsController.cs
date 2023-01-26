379b030dfbf83c9606e1f8b2b822c2f4e5b85f3f
Satria Primatama <satriaprimatama88@gmail.com>
2023-01-26 15:33:01 +0700

    Revert "Update View Model Master Job Placement and Fixed Bug Job Management"
    This reverts commit 0337609909318a80658719b06672cabe470c7efe.

--- Api/Controllers/JobPlacementsController.cs
+++ Api/Controllers/JobPlacementsController.cs
@@ -12,29 +12,8 @@ namespace Api.Controllers
     /*[Authorize(Roles = "Manager")]*/
     public class JobPlacementsController : BaseController<JobPlacementRepositories, JobPlacements, int>
     {
-        private readonly JobPlacementRepositories _repo;
-
         public JobPlacementsController(JobPlacementRepositories repo) : base(repo)
         {
-            _repo = repo;
-        }
-
-        [HttpGet]
-        [Route("JobPlacementMaster")]
-        public ActionResult JobPlacementMaster()
-        {
-            try
-            {
-                var result = _repo.GetMaster();
-                return result.Count() == 0
-                ? Ok(result)
-                : Ok(result);
-            }
-            catch (Exception e)
-            {
-                return BadRequest(new { statusCode = 500, message = $"Something Wrong! : {e.Message}" });
-            }
-
         }
     }
 }
--- Api/Repositories/Data/JobPlacementRepositories.cs
+++ Api/Repositories/Data/JobPlacementRepositories.cs
@@ -1,30 +1,11 @@
 ﻿using Api.Contexts;
 using Api.Models;
-using Api.ViewModels;
 
 namespace Api.Repositories.Data;
 
 public class JobPlacementRepositories : GeneralRepository<JobPlacements, int>
 {
-    private readonly MyContext _context;
-
     public JobPlacementRepositories(MyContext context) : base(context)
     {
-        _context = context;
-    }
-
-    public IEnumerable<MJobPlacementVM> GetMaster()
-    {
-        return (from jp in _context.JobPlacements
-                join e in _context.Employees on jp.EmployeeId equals e.Id
-                join d in _context.Departments on jp.DepartmentId equals d.Id
-                join j in _context.Jobs on jp.JobId equals j.Id
-                select new MJobPlacementVM
-                {
-                    Name = e.FirstName + " " + e.LastName,
-                    Department = d.Name,
-                    Job = j.Name,
-                    Id = jp.Id
-                }).ToList();
     }
 }
