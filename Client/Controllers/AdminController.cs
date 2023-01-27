d944c5fc4816aca72d4cdd85b340a4d8a4604210
Satria Primatama <satriaprimatama88@gmail.com>
2023-01-27 11:17:53 +0700

    Update Manager Role

--- Client/Controllers/AdminController.cs
+++ Client/Controllers/AdminController.cs
@@ -1,37 +1,43 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Api.Models;
+using Client.Base;
+using Client.Repositories.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Client.Controllers;
-[Authorize(Roles = "Admin")]
-public class AdminController : BaseController<Accounts, AccountRepository, int>
+namespace Client.Controllers
 {
+    [Authorize]
 
-    private readonly AccountRepository repository;
-    public AdminController(AccountRepository repository) : base(repository)
+    public class AdminController : BaseController<Accounts, AccountRepository, int>
     {
-        this.repository = repository;
-    }
+        private readonly AccountRepository repository;
+        public AdminController(AccountRepository repository) : base(repository)
+        {
+            this.repository = repository;
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        [Authorize(Roles = "Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    //CreateAccount
-    public IActionResult CreateAccount()
-    {
-        return View();
-    }
+        //CreateAccount
+        public IActionResult CreateAccount()
+        {
+            return View();
+        }
 
-    //CreateRole
-    public IActionResult CreateRole()
-    {
-        return View();
-    }
+        //CreateRole
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
 
-    //Total Time Off Employee
-    public IActionResult TotalTimeOff()
-    {
-        return View();
+        //Total Time Off Employee
+        public IActionResult TotalTimeOff()
+        {
+            return View();
+        }
     }
 }
